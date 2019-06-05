784e44dab2c57a37ce9b63c41bb199f35296754a
Alberto Siletti <alberto.siletti@gmail.com>
2019-06-05 11:01:52 +0200

    Initial commit

---
+++ src/main/java/com/siletti/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,37 @@
+package com.siletti.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+public class ExchangeValue {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversion;
+
+    public ExchangeValue() {
+    }
+
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversion) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversion = conversion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversion() {
+        return conversion;
+    }
+}
