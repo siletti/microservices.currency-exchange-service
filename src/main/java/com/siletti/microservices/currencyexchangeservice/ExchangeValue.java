2d72b696e15660d54e372494cc4d46d7b3e45659
siletti <alberto.siletti@gmail.com>
2019-06-06 11:21:07 +0200

    add JPA & H2

--- src/main/java/com/siletti/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/siletti/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,11 +1,18 @@
 package com.siletti.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class ExchangeValue {
 
+    @Id
     private Long id;
+    @Column(name="cur_from")
     private String from;
+    @Column(name="cur_to")
     private String to;
     private BigDecimal conversion;
     private int port;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into Exchange_Value(id,cur_from,cur_to,conversion,port)
+values(1001,'USD','INR',65,0);
+insert into Exchange_Value(id,cur_from,cur_to,conversion,port)
+values(1002,'EUR','INR',75,0);
+insert into Exchange_Value(id,cur_from,cur_to,conversion,port)
+values(1003,'AUD','INR',25,0);
